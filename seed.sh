bac41d292200895352ac6aaa0a5353221bd794d2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-07-19 18:21:36 +0800

    fixing translate part

--- seed.sh
+++ seed.sh
@@ -51,14 +51,16 @@ fi
 # ---- main body ----
 echo -n "app name : ";
 read app_name;
-ionic start "$app_name" sidemenu --v2
+#ionic start "$app_name" sidemenu --v2
+rm -rf "$app_name";
+cp -rf seed "$app_name";
 
 cp add_page.sh "$app_name/";
 cd "$app_name"
 chmod +x add_page.sh
 
 # set translate stuff
-npm install --save ng2-translate
+#npm install --save ng2-translate
 mkdir -p "www/assets/i18n";
 cd "www/assets/i18n";
 echo "{
@@ -73,18 +75,45 @@ import {provide} from '@angular/core';
 import {Http, HTTP_PROVIDERS} from '@angular/http';
 import {TranslateService, TranslatePipe, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
 " | cat - app.ts | tail -n +2 | head -n -1 > temp && mv temp app.ts;
-sed -i "14i  , pipes: [TranslatePipe]" app.ts;
+sed -i "/templateUrl: /a \  , pipes: [TranslatePipe]" app.ts;
 echo "ionicBootstrap(MyApp, [[provide(TranslateLoader, {
   useFactory: (http:Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
   deps: [Http]
 }), TranslateService]], {});" >> app.ts
+sed -i "/this.initializeApp();/a \ \ \ \ this.translateConfig();" app.ts;
+sed -i "/  initializeApp/i \
+\ \ translateConfig()\{\
+\n    let userLang = navigator.language.split('-')[0];\
+\n    userLang = /(zh|ch|en)/gi.test(userLang) ? userLang : 'en';\
+\n\
+\n    this.translate.setDefaultLang('en');\
+\n    this.translate.use(userLang);\
+\n\
+\n    console.debug('userLang', userLang);\
+\n  }\n" app.ts;
+exit 0;
+sed i "/ initializeApp\(\)/i \
+  translateConfig() {
+    let userLang = navigator.language.split('-')[0];
+    userLang = /(zh|ch|en)/gi.test(userLang) ? userLang : 'en';
+
+    this.translate.setDefaultLang('en');
+    this.translate.use(userLang);
+
+    console.debug('userLang', userLang);
+  }
+" app.ts;
 cd "../";
 
 # init demo page
 echo "Demo" | ./add_page.sh "Page1";
 cd "app/pages/demo";
 sed -i 's/Page Uno/{{ "DEMO" | translate }}/' demo.html;
-cd "../../../";
+cd "../../"
+#sed -i '/component: Page/d' app.ts
+sed -i "11i import { Demo } from './pages/demo/demo';" app.ts;
+sed -i "31i  , {title: \"DEMO\", component: Demo}" app.ts;
+cd "../";
 
 echo "";
 echo "All Done.";
